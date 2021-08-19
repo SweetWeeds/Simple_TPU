8e36b970a44c79c549171462a4592246d1feb512
SweetWeeds <khk4502@gmail.com>
2021-08-19 18:22:47 +0900

    QThread Applied

--- GUI_SW/main.py
+++ GUI_SW/main.py
@@ -1,8 +1,10 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 from PyQt5 import uic
 from serial import Serial
+import serial
 import torch
 from torchvision import datasets, transforms
 import random
@@ -11,6 +13,7 @@ import qimage2ndarray
 import os
 from scipy.special import log_softmax
 
+serial_inst = None
 transform = transforms.Compose([transforms.ToTensor(),
                               transforms.Normalize((0.5,), (0.5,)),
                               ])
@@ -71,6 +74,89 @@ def GetRandomImage(img=None, label=None):
 
     return img, label
 
+def getUartData(x, Q):
+        ret = np.trunc(x*(2**Q)).astype(int)
+        ret = np.clip(ret, -128, 127)
+        ret = ret.flatten()
+        return ret
+
+
+def Inference(img_uart, serial_inst):
+    test_input = img_uart
+    serial_inst.reset_output_buffer()
+    serial_inst.reset_input_buffer()
+    for i in range(0, 784, 16):
+        tmp = encode(np.flip(test_input[i:i+16]), 16, 8)
+        serial_inst.write(tmp.encode('ascii'))
+        while serial_inst.inWaiting() == 0:
+            continue
+        while serial_inst.inWaiting() > 0:
+            resp = serial_inst.read()
+            # Response Check
+            if (resp != b'\x01'):
+                print(f"[ERROR] Resp value is not correct ({resp})")
+
+    # Get Calculated Data
+    result = ""
+    while (len(result) < 32):
+        while serial_inst.inWaiting() == 0:
+            continue
+        while serial_inst.inWaiting() > 0:
+            tmp = serial_inst.read()
+            #print(f"tmp:{tmp}")
+            result += tmp.decode("ascii")
+    
+    x = np.flip(decode(result, 16, 8))
+    x = fixed_to_point(x, 4)
+    x = log_softmax(x)
+
+    ps = torch.exp(torch.tensor(x))
+    probab = list(ps.to("cpu").numpy())
+    pred_label = probab.index(max(probab))
+    #print(f"Pred:{pred_label}, True:{self.label}")
+    return pred_label
+
+
+
+class BenchmarkQThread(QThread):
+    img_refresh_event = pyqtSignal(list)
+    pred_event = pyqtSignal(list)
+    benchmark_complete_event = pyqtSignal(list)
+    def __init__(self, parent=None):
+        super().__init__()
+
+    def run(self):
+        global serial_inst
+        correct_count, all_count = 0, 0
+        entire_count = len(valloader)*64
+        for images,labels in valloader:
+            for i in range(len(labels)):
+                img = images[i].view(1, 784).to("cpu").numpy()
+                label = labels.numpy()[i]
+                
+                self.img_refresh_event.emit([img, label])
+
+                #self.RefreshImg()
+                #self.getUartData(self.img, 4)
+                img_uart = getUartData(img, 4)
+                pred_label = Inference(img_uart=img_uart, serial_inst=serial_inst)
+
+                if(label == pred_label):
+                    correct_count += 1
+                all_count += 1
+
+                self.pred_event.emit([pred_label, all_count, entire_count, correct_count])
+
+        accuracy = (correct_count/all_count)
+        print("Number Of Images Tested =", all_count)
+        print("\nModel Accuracy =", accuracy)
+        self.benchmark_complete_event.emit([all_count, accuracy])
+    
+    def stop(self):
+        self.working = False
+        self.quit()
+        self.wait(5000)
+
 
 class WindowClass(QMainWindow, form_class) :
     def __init__(self) :
@@ -79,11 +165,13 @@ class WindowClass(QMainWindow, form_class) :
         self.initUI()
         self.PORT = "COM4"
         self.BAUDRATE = 115200
-        self.serial_inst = None
+        #self.serial_inst = None
         self.label = None
         self.img = None
         self.byte_img = None
         self.img_uart = None
+        self.correct_count = 0
+        self.all_count = 0
 
     def initUI(self):
         self.pushButton.clicked.connect(self.SerialConnect)
@@ -93,12 +181,20 @@ class WindowClass(QMainWindow, form_class) :
         #self.pushButton_3.clicked.connect(self.Test)
         self.pushButton_4.setEnabled(False)
         self.pushButton_4.clicked.connect(self.Benchmark)
+        self.progressBar.setValue(0)
+        self.progressBar.setValue(150)
+        self.bench_th = BenchmarkQThread()
+        self.bench_th.benchmark_complete_event.connect(self.Bench_Complete)
+        self.bench_th.pred_event.connect(self.Bench_Pred)
+        self.bench_th.img_refresh_event.connect(self.Bench_Img_Refresh)
+        self.bench_en = False
 
     def SerialConnect(self):
+        global serial_inst
         mode = self.pushButton.text()
         if (mode == "Connect"):
             try:
-                self.serial_inst = Serial(self.PORT, self.BAUDRATE, timeout=None)
+                serial_inst = Serial(self.PORT, self.BAUDRATE, timeout=None)
                 self.pushButton_3.setEnabled(True)
                 self.pushButton_4.setEnabled(True)
                 self.pushButton.setText("Disconnect")
@@ -114,8 +210,8 @@ class WindowClass(QMainWindow, form_class) :
                 msg.exec_()
         else:
             try:
-                self.serial_inst.close()
-                self.serial_inst = None
+                serial_inst.close()
+                serial_inst = None
                 self.pushButton_3.setEnabled(False)
                 self.pushButton_4.setEnabled(False)
                 self.pushButton.setText("Connect")
@@ -138,11 +234,7 @@ class WindowClass(QMainWindow, form_class) :
         self.getUartData(self.img, 4)
 
     def getUartData(self, x, Q):
-        ret = np.trunc(x*(2**Q)).astype(int)
-        ret = np.clip(ret, -128, 127)
-        ret = ret.flatten()
-        self.img_uart = ret
-        return ret
+        self.img_uart = getUartData(x, Q)
     
     def getByteImage(self, img):
         ret = img + 1
@@ -160,24 +252,25 @@ class WindowClass(QMainWindow, form_class) :
         self.graphicsView.setScene(scene)
 
     def Test(self):
+        global serial_inst
         # Write Input Data
         for i in range(0, 784, 16):
             tmp = encode(np.flip(TEST_DATA[i:i+16]), 16, 8)
-            self.serial_inst.write(tmp.encode('ascii'))
-            while self.serial_inst.inWaiting() == 0:
+            serial_inst.write(tmp.encode('ascii'))
+            while serial_inst.inWaiting() == 0:
                 continue
-            while self.serial_inst.inWaiting() > 0:
-                resp = self.serial_inst.read()
+            while serial_inst.inWaiting() > 0:
+                resp = serial_inst.read()
                 if (resp != b'\x01'):
                     print(f"[ERROR] Resp value is not correct ({resp})")
         
         # Get Calculated Data
         result = ""
         while (len(result) < 32):
-            while self.serial_inst.inWaiting() == 0:
+            while serial_inst.inWaiting() == 0:
                 continue
-            while self.serial_inst.inWaiting() > 0:
-                tmp = self.serial_inst.read()
+            while serial_inst.inWaiting() > 0:
+                tmp = serial_inst.read()
                 #print(f"tmp:{tmp}")
                 result += tmp.decode("ascii")
         
@@ -191,60 +284,41 @@ class WindowClass(QMainWindow, form_class) :
         print(f"Pred:{pred_label}")
 
     def Inference(self):
-        #test_input = np.array([i for i in range(784)])
-        test_input = self.img_uart
-        self.serial_inst.reset_output_buffer()
-        self.serial_inst.reset_input_buffer()
-        for i in range(0, 784, 16):
-            tmp = encode(np.flip(test_input[i:i+16]), 16, 8)
-            self.serial_inst.write(tmp.encode('ascii'))
-            while self.serial_inst.inWaiting() == 0:
-                continue
-            while self.serial_inst.inWaiting() > 0:
-                resp = self.serial_inst.read()
-                if (resp != b'\x01'):
-                    print(f"[ERROR] Resp value is not correct ({resp})")
-
-        # Get Calculated Data
-        result = ""
-        while (len(result) < 32):
-            while self.serial_inst.inWaiting() == 0:
-                continue
-            while self.serial_inst.inWaiting() > 0:
-                tmp = self.serial_inst.read()
-                #print(f"tmp:{tmp}")
-                result += tmp.decode("ascii")
-        
-        x = np.flip(decode(result, 16, 8))
-        x = fixed_to_point(x, 4)
-        x = log_softmax(x)
-
-        ps = torch.exp(torch.tensor(x))
-        probab = list(ps.to("cpu").numpy())
-        pred_label = probab.index(max(probab))
+        global serial_inst
+        pred_label = Inference(getUartData(self.img, 4), serial_inst)
         print(f"Pred:{pred_label}, True:{self.label}")
+        self.PredlabelEdit.setText(f"{pred_label}")
         return pred_label
     
+    @pyqtSlot()
     def Benchmark(self):
-        correct_count, all_count = 0, 0
-        for images,labels in valloader:
-            for i in range(len(labels)):
-                self.img = images[i].view(1, 784).to("cpu").numpy()
-                self.label = labels.numpy()[i]
-                self.RefreshImg()
-                self.getUartData(self.img, 4)
+        if (self.bench_en == False):
+            self.pushButton_4.setText("Stop")
+            self.bench_th.start()
+        else:
+            self.pushButton_4.setText("Benchmark")
+            self.bench_th.stop()
 
-                pred_label = self.Inference()
-                if(self.label == pred_label):
-                    correct_count += 1
-                all_count += 1
-        
-        print("Number Of Images Tested =", all_count)
-        print("\nModel Accuracy =", (correct_count/all_count))
+    def Bench_Img_Refresh(self, val):
+        self.img = val[0]
+        self.label = val[1]
+        self.RefreshImg()
+    
+    def Bench_Pred(self, val):
+        self.PredlabelEdit.setText(f"{val[0]}")
+        self.progressBar.setValue(val[1])
+        self.progressBar.setMaximum(val[2])
+        self.ProgressLabel.setText(f"{val[1]}/{val[2]}")
+        acc = val[3]/val[1]
+        self.AcclineEdit.setText('%.3f' % acc)
+    
+    def Bench_Complete(self, val):
+        self.progressBar.setValue(val[0])
+        self.progressBar.setMaximum(val[0])
+        self.AcclineEdit.setText(f"val[1]")
 
 
 if __name__ == "__main__" :
-    #QApplication : 프로그램을 실행시켜주는 클래스
     app = QApplication(sys.argv) 
 
     myWindow = WindowClass() 
